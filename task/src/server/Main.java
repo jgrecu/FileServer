706ec8dfc9ab70da24f98bb567c81822c76373c8
Jeremy Grecu <jeremy.grecu@sap.com>
2021-12-10 13:06:50 +0000

    JBA File Server - Stage 1/4: Storage emulator

---
+++ task/src/server/Main.java
@@ -0,0 +1,11 @@
+package server;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.run();
+
+    }
+}
---
+++ task/src/server/Menu.java
@@ -0,0 +1,61 @@
+package server;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Menu {
+    private final Scanner scanner = new Scanner(System.in);
+    private final Set<String> storage = new HashSet<>();
+
+    public void run() {
+        while (true) {
+            String input = scanner.nextLine().strip();
+            if ("exit".equals(input)) {
+                scanner.close();
+                return;
+            } else {
+                String[] parts = input.split("\\s+");
+                String command = parts[0];
+                String fileName = parts.length > 1 ? parts[1] : null;
+                switch (command) {
+                    case "add":
+                        addFile(fileName);
+                        break;
+                    case "get":
+                        getFile(fileName);
+                        break;
+                    case "delete":
+                        deleteFile(fileName);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private void deleteFile(String fileName) {
+        if (fileName != null && storage.remove(fileName)) {
+            System.out.println("The file " + fileName + " was deleted");
+        } else {
+            System.out.println("The file " + fileName + " not found");
+        }
+    }
+
+    private void getFile(String fileName) {
+        if (fileName != null && storage.contains(fileName)) {
+            System.out.println("The file " + fileName + " was sent");
+        } else {
+            System.out.println("The file " + fileName + " not found");
+        }
+    }
+
+    private void addFile(String fileName) {
+        if (fileName != null && fileName.matches("file[1-9]0?") && storage.add(fileName)) {
+            System.out.println("The file " + fileName + " added successfully");
+        } else {
+            System.out.println("Cannot add the file " + fileName);
+        }
+    }
+}

cd015b09f42fd808643a3daff0d2c3b0e1cb3e17
Jeremy Grecu <jeremy.grecu@sap.com>
2021-12-11 12:25:23 +0000

    JBA File Server - cleaned up server/Main

--- task/src/server/Main.java
+++ task/src/server/Main.java
@@ -3,11 +3,7 @@ package server;
 public class Main {
 
     public static void main(String[] args) {
-
-        /*Menu menu = new Menu();
-        menu.run();*/
         Server server = new Server();
         server.start();
-
     }
 }
